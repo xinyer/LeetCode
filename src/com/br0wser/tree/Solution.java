79862ecc9efd30aab7de1cc067888aafc676b74c
xinyer <xinyer0510@gmail.com>
2016-03-04 16:57:47 +0800

    判断两个二叉树是否相等

--- src/com/br0wser/tree/Solution.java
+++ src/com/br0wser/tree/Solution.java
@@ -20,4 +20,48 @@ public class Solution {
         int rightDepth = maxDepth(root.right);
         return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
     }
+
+    /**
+     * NO. 226 Invert Binary Tree My Submissions Question
+     * Invert a binary tree.
+
+            4
+          /   \
+        2     7
+      / \   / \
+     1   3 6   9
+
+     to
+
+            4
+          /   \
+        7     2
+      / \   / \
+     9   6 3   1
+
+     */
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) return root;
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    /**
+     * NO. 100 Same Tree My Submissions Question
+     * Given two binary trees, write a function to check if they are equal or not.
+     * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        return((p==null||q==null)?(p==q):(p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)));
+    }
+
 }
